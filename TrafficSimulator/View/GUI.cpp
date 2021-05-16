38bc7f575b16683d5a3306f281e1d0091b4abbfb
Sándor Balázs <it.sandor.balazs@gmail.com>
2021-05-16 17:14:20 +0200

    GUI update

--- TrafficSimulator/View/GUI.cpp
+++ TrafficSimulator/View/GUI.cpp
@@ -3,10 +3,47 @@
 GUI::GUI(void) {
 
 }
+
 GUI::~GUI(void) {
 
 }
 
-void GUI::init(SDL_Window* window) {
-	ImGui_ImplSdlGL3_Init(window);
+bool GUI::init(SDL_Window* window) {
+	return ImGui_ImplSdlGL3_Init(window);
+}
+
+void GUI::clean() {
+	ImGui_ImplSdlGL3_Shutdown();
+}
+
+void GUI::bind(WorkWindow* currentWindow) {
+	workingWindow = currentWindow;
+}
+
+void GUI::eventHandler(SDL_Event& event) {
+	ImGui_ImplSdlGL3_ProcessEvent(&event);
+}
+
+bool GUI::isKeyboardCaptured() {
+	return ImGui::GetIO().WantCaptureMouse;
+}
+
+bool GUI::isMouseCaptured() {
+	return ImGui::GetIO().WantCaptureKeyboard;
+}
+
+void GUI::guiDraw() {
+	ImGui::SetNextWindowPos(ImVec2(0, 0), ImGuiSetCond_FirstUseEver);
+	ImGui::SetNextWindowSize(ImVec2(500, 240), ImGuiCond_FirstUseEver);
+	if (ImGui::Begin("Rigid Body Simulation")) {
+		ImGui::Text("Rigid Body Simulation");
+		ImGui::Text("By: Sandor Balazs");
+	}
+	ImGui::End();
+}
+
+void GUI::render(SDL_Window* window) {
+	guiDraw();
+	ImGui_ImplSdlGL3_NewFrame(window);
+	ImGui::Render();
 }
--- TrafficSimulator/View/GUI.h
+++ TrafficSimulator/View/GUI.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <SDL.h>
-
+#include "WorkWindow.h"
 #include "imgui\imgui.h"
 #include "imgui\imgui_impl_sdl_gl3.h"
 
@@ -8,6 +8,16 @@ class GUI {
 public:
 	GUI(void);
 	~GUI(void);
-	void init(SDL_Window* window);
+	static bool init(SDL_Window* window);
+	static void clean();
+	void bind(WorkWindow* currentWindow);
+	void guiDraw();
+	void render(SDL_Window* window);
+	void eventHandler(SDL_Event& event);
+	bool isKeyboardCaptured();
+	bool isMouseCaptured();
+
 private:
+	WorkWindow* workingWindow;
+	SDL_Window* window;
 };
