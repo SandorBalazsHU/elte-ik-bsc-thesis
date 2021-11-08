4d365b70fcc7b6fc85e685cb58ed981f38afdbd6
Balázs Sándor <sabuaai@INF.ELTE.HU>
2021-11-08 21:34:00 +0100

    Documentation adding.

--- TrafficSimulator/TrafficSimulator.cpp
+++ TrafficSimulator/TrafficSimulator.cpp
@@ -1,8 +1,23 @@
+/**
+ * @name Traffic Simulation
+ * @file Camera.cpp
+ * @class TrafficSimulator
+ * @author Sándor Balázs - AZA6NL
+ * @date 2021.11.08.
+ * @brief Main file.
+ * Contact: sandorbalazs9402@gmail.com
+*/
 #include <iostream>
 
 #include "View/WorkWindow.h"
 #include "Control/Logger.h"
 
+/**
+ * @brief Program main function.
+ * @param argc Consol Argument count.
+ * @param args Cosol arguments array.
+ * @return program exit status.
+*/
 int main(int argc, char* args[])
 {
     Logger::startFileLogging();
--- TrafficSimulator/View/WorkWindow.h
+++ TrafficSimulator/View/WorkWindow.h
@@ -1,3 +1,13 @@
+/**
+ * @name Traffic Simulation
+ * @file WorkingWindow.h
+ * @class WorkingWindow
+ * @author Sándor Balázs - AZA6NL
+ * @date 2021.11.08.
+ * @brief Opengl 3D system initialisation and window open.
+ * Contact: sandorbalazs9402@gmail.com
+*/
+
 #pragma once
 
 #include <SDL.h>
@@ -10,81 +20,248 @@
 #include "Utilities/Mesh_OGL3.h"
 #include "Objects/ObjectStorage.h"
 
+/**
+ * @brief The OpenGL 3D window implementation
+*/
 class WorkWindow {
 public:
+
+	/**
+	 * @brief OpenGL 3D Window konstructor.
+	*/
 	WorkWindow(void);
+
+	/**
+	 * @brief OpenGL 3D Window destructor.
+	*/
 	~WorkWindow(void);
+
+	/**
+	 * @brief Open and show the window. This run all the preparation function and start the rendering.
+	 * @return The starting process status number. 0 is OK Other ID-s mean errors.
+	*/
 	int open();
 
+	/**
+	 * @brief Stop the render and start the closeing procedure.
+	*/
 	void close() {
 		exit = true;
 	};
 
+	/**
+	 * @brief Getter for the camera controller.
+	 * @return pointer for the camera controller.
+	*/
 	Camera* getCamera() {
 		return &camera;
 	};
 
+	/**
+	 * @brief Getter for the 3D Window compiled shader program.
+	 * @return The OpõenGL 3D Shader.
+	*/
 	ProgramObject* getShader() {
 		return &shader;
 	}
 
+	/**
+	 * @brief Getter for the SDL Windows object.
+	 * @return The SDL OpenGL window object.
+	*/
 	SDL_Window* getWindow() {
 		return window;
 	}
 
+	/**
+	 * @brief Getter for the window title.
+	 * @return The window title.
+	*/
 	std::string getWindowTitle() {
 		return windowTitle;
 	}
 
+	/**
+	 * @brief Getter for the GUI representer.
+	 * @return Pointer to the GUI controller class.
+	*/
 	GUI* getGUI() {
 		return &gui;
 	}
 
+	/**
+	 * @brief Getter for the object storage.
+	 * @return Pointer to the object storage.
+	*/
 	ObjectStorage* getObjectStorage() {
 		return &objectStorage;
 	}
 
+	/**
+	 * @brief Getter for the render object.
+	 * @return Pointer to the current render.
+	*/
 	Render* getRender() {
 		return &render;
 	}
 
+	/**
+	 * @brief Getter for the event listener.
+	 * @return Pointer for the current eventlistener.
+	*/
 	EventListener* getEventListener() {
 		return &eventListener;
 	}
 
 private:
+
+	/**
+	 * @brief The main window default tittle text.
+	*/
 	const char* windowDefTitle = "Traffic Simulator";
+
+	/**
+	 * @brief The default windows with.
+	*/
 	const int windowDefSizeX = 640;
+
+	/**
+	 * @brief The default window height.
+	*/
 	const int windowDefSizeY = 480;
+
+	/**
+	 * @brief The default window horisontal position.
+	*/
 	const int windowDefPosX = 100;
+
+	/**
+	 * @brief The default window vertical position.
+	*/
 	const int windowDefPosY = 100;
 
+	/**
+	 * @brief The main window base tittle text.
+	*/
 	std::string windowTitle = "Traffic simulator v1.0";
 
+	/**
+	 * @brief The SDL window pointer.
+	*/
 	SDL_Window*		window;
+
+	/**
+	 * @brief The openGL context object.
+	*/
 	SDL_GLContext	context;
+
+	/**
+	 * @brief The shader object.
+	*/
 	ProgramObject	shader;
+
+	/**
+	 * @brief The camera controller object.
+	*/
 	Camera			camera;
+
+	/**
+	 * @brief The event listener object.
+	*/
 	EventListener	eventListener;
+
+	/**
+	 * @brief The render object.
+	*/
 	Render			render;
+
+	/**
+	 * @brief The GUI controller object.
+	*/
 	GUI				gui;
+
+	/**
+	 * @brief The 3D Object storage object.
+	*/
 	ObjectStorage	objectStorage;
 
+	/**
+	 * @brief The render loop running parameter.
+	*/
 	bool exit = false;
 
+	/**
+	 * @brief SDL system initialisation.
+	 * @return Status ID. 0 is OK status. 
+	*/
 	int sdlInit();
+
+	/**
+	 * @brief OpenGL preconfiguration.
+	 * @return Status ID. 0 is OK status.
+	*/
 	int openGLpreConfig();
+
+	/**
+	 * @brief SDL window opening.
+	 * @return Status ID. 0 is OK status.
+	*/
 	int openSDLWindow();
+
+	/**
+	 * @brief inGUI GUS system start.
+	 * @return Status ID. 0 is OK status.
+	*/
 	int imGUIinit();
+
+	/**
+	 * @brief OpenGL Context creation.
+	 * @return Status ID. 0 is OK status.
+	*/
 	int createOpenGLContext();
+
+	/**
+	 * @brief OpenGL Variables setting.
+	 * @return Status ID. 0 is OK status.
+	*/
 	int openGLpostConfig();
+
+	/**
+	 * @brief GLEW System start.
+	 * @return Status ID. 0 is OK status.
+	*/
 	int glewStart();
+
+	/**
+	 * @brief OpenGL Shader configurationvand compiling.
+	 * @return Status ID. 0 is OK status.
+	*/
 	int shaderConfig();
+
+	/**
+	 * @brief Render variable setting.
+	 * @return Status ID. 0 is OK status.
+	*/
 	int renderPreconfig();
+
+	/**
+	 * @brief Render Start!
+	 * @return Status ID. 0 is OK status.
+	*/
 	int renderStart();
 
+	/**
+	 * @brief Clear the screen
+	*/
 	void clearScreen();
 
+	/**
+	 * @brief Cleanup before closing.
+	*/
 	void cleanup();
+
+	/**
+	 * @brief Windows close callback function.
+	*/
 	static void exitWindow();
 };
