86f5e59e45364aa595537cd626f68689807abf38
Balázs Sándor <sabuaai@INF.ELTE.HU>
2021-12-10 02:43:16 +0100

    Documentation.

--- TrafficSimulator/View/GUI.cpp
+++ TrafficSimulator/View/GUI.cpp
@@ -224,8 +224,6 @@ void GUI::draw() {
 				animator->finalize();
 				if (animator->getGraph()->getEndPoints().size() > 0 && animator->getGraph()->getStartPoints().size() > 0) {
 					editorLock = true;
-					//TODO: Save as után elszáll pointer hibával.
-					//Módosítás után elszáll???
 					windowRender->lockEditor();
 					mapEditorWindow = false;
 					simulationWindow = true;
