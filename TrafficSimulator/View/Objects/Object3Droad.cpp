4e5727c2bbd078d8d796b22d7d50d50a993d380c
Balázs Sándor <sabuaai@INF.ELTE.HU>
2021-12-15 15:42:15 +0100

    Styling.

--- TrafficSimulator/View/Objects/Object3Droad.cpp
+++ TrafficSimulator/View/Objects/Object3Droad.cpp
@@ -1,24 +1,59 @@
+/**
+ * @name Traffic Simulation
+ * @file Object3Droad.cpp
+ * @class Object3Droad
+ * @author Sándor Balázs - AZA6NL
+ * @date 2021.12.15.
+ * @brief The parametric generated road implementation.
+ * Contact: sandorbalazs9402@gmail.com
+*/
+
 //Tendi
+
 #include "Object3Droad.h"
 #include "../Render.h"
 #include <glm/gtx/transform2.hpp>
 #include <map>
 
+/**
+ * @brief The Object3Droad constructor. Set the basic variables too.
+*/
 Object3Droad::Object3Droad() {
 	roadYposition = 0.1f;
 	color = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
 };
 
+/**
+ * @brief Object3Droad copy constructor.
+ * @param The copyable road.
+*/
 Object3Droad::Object3Droad(const Object3Droad&) {}
 
-Object3Droad::~Object3Droad(void) {
-	//std::cout << "DESTRUCTOR" << std::endl;
-}
+/**
+ * @brief Destructor.
+ * @param Empty.
+*/
+Object3Droad::~Object3Droad(void) {}
 
+/**
+ * @brief Getter for the road render it.
+ * @return The road render it.
+*/
 size_t  Object3Droad::getRenderID() {
 	return renderID;
 }
 
+/**
+ * @brief Bind this road to the render and initialize the road, generate the 3D model and initialize the connected objects.
+ * @param render The current render pointer.
+ * @param newRenderID The current render ID.
+ * @param trackBall_0 The trackBall_0 object ID.
+ * @param trackBall_1 The trackBall_1 object ID.
+ * @param trackBall_2 The trackBall_2 object ID.
+ * @param trackBall_3 The trackBall_3 object ID.
+ * @param roadEndCircle_1 The roadEndCircle_1 object ID.
+ * @param roadEndCircle_2 The roadEndCircle_2 object ID.
+*/
 void Object3Droad::bind(Render* render, size_t newRenderID, size_t trackBall_0, size_t trackBall_1, size_t trackBall_2, size_t trackBall_3, size_t roadEndCircle_1, size_t roadEndCircle_2) {
 	this->render = render;
 	this->renderID = newRenderID;
@@ -58,6 +93,15 @@ void Object3Droad::bind(Render* render, size_t newRenderID, size_t trackBall_0, 
 	render->getObject(roadEndCircles[1])->setPosition(glm::vec3(points[points.size() - 1].x, roadYposition + 0.05f, points[points.size() - 1].z));
 }
 
+/**
+ * @brief Rebind the connected objects after changes. 
+ * @param trackBall_0 The trackBall_0 object ID.
+ * @param trackBall_1 The trackBall_1 object ID.
+ * @param trackBall_2 The trackBall_2 object ID.
+ * @param trackBall_3 The trackBall_3 object ID.
+ * @param roadEndCircle_1 The roadEndCircle_1 object ID.
+ * @param roadEndCircle_2 The roadEndCircle_2 object ID.
+*/
 void Object3Droad::reBind(size_t trackBall_0, size_t trackBall_1, size_t trackBall_2, size_t trackBall_3, size_t roadEndCircle_1, size_t roadEndCircle_2) {
 	trackBalls[0] = trackBall_0;
 	trackBalls[1] = trackBall_1;
@@ -68,6 +112,9 @@ void Object3Droad::reBind(size_t trackBall_0, size_t trackBall_1, size_t trackBa
 	updateBasepoints();
 }
 
+/**
+ * @brief Update the base points position for the Bézier curve and do the endpoint and marker tests.
+*/
 void Object3Droad::updateBasepoints() {
 	endpointsExistTest();
 	markersExistTest();
@@ -84,7 +131,13 @@ void Object3Droad::updateBasepoints() {
 	for (size_t i = 0; i < 4; i++) basepoints[i] = render->getObject(trackBalls[i])->getPosition();
 }
 
-//TODO Áthozni a felújított verziót.
+//TODO Update the hit checker version
+/**
+ * @brief The road click checker. Return true if a ray intersects the road hit one of the spheres.
+ * @param cameraPosition The current camera position.
+ * @param ray The ray for the intersection.
+ * @return Return true if a ray intersects the road hit one of the spheres.
+*/
 bool Object3Droad::isClicked(glm::vec3 cameraPosition, glm::vec3 ray) {
 	for (size_t i = 1; i < points.size(); i++) {
 		glm::vec3 hitSphereCenter = points[i];
@@ -102,10 +155,18 @@ bool Object3Droad::isClicked(glm::vec3 cameraPosition, glm::vec3 ray) {
 	return false;
 }
 
+/**
+ * @brief Setter for the road render color with opacity.
+ * @param RGBAcolor The road render color  with opacity.
+*/
 void Object3Droad::setRGBAcolor(glm::vec4 RGBAcolor) {
 	color = RGBAcolor;
 }
-//TODO A lemezekre is átvinni a változásokat
+
+/**
+ * @brief Setter for the road render color without opacity.
+ * @param RGBcolor The road render color without opacity.
+*/
 void Object3Droad::setRGBcolor(glm::vec3 RGBcolor) {
 	color.x = RGBcolor.x;
 	color.y = RGBcolor.y;
@@ -113,30 +174,59 @@ void Object3Droad::setRGBcolor(glm::vec3 RGBcolor) {
 	render->getObject(roadEndCircles[0])->setRGBcolor(RGBcolor);
 	render->getObject(roadEndCircles[1])->setRGBcolor(RGBcolor);
 }
+
+/**
+ * @brief Set road render opacity.
+ * @param opacity The road render opacity.
+*/
 void Object3Droad::setOpacity(float opacity) {
 	color.w = opacity;
 }
 
+/**
+ * @brief Calculate for the road world matrix for the road rendering.
+ * @return The road world matrix for the road rendering.
+*/
 glm::mat4 Object3Droad::getWorldMatrix() {
 	return glm::translate(glm::vec3(0, 0, 0));
 }
 
+/**
+ * @brief Getter for the road render color with opacity.
+ * @return The road render color with opacity.
+*/
 glm::vec4 Object3Droad::getRGBAcolor() {
 	return color;
 }
 
+/**
+ * @brief Getter for the road render color without opacity.
+ * @return The road render color without opacity.
+*/
 glm::vec3 Object3Droad::getRGBcolor() {
 	return glm::vec3(color.y, color.z, color.w);
 }
 
+/**
+ * @brief Getter for the road opacity.
+ * @return The road opacity.
+*/
 float Object3Droad::getOpacity() {
 	return color.w;
 }
 
+/**
+ * @brief Checker for the selected status flag.
+ * @return Return true if the road is in selected status.
+*/
 bool Object3Droad::isSelected() {
 	return selected;
 }
 
+/**
+ * @brief Calculate the road exact length.
+ * @return The road exact length.
+*/
 float Object3Droad::getRoadLength() {
 	float length = 0.0f;
 	for (size_t i = 1; i < points.size(); i++) {
@@ -145,15 +235,25 @@ float Object3Droad::getRoadLength() {
 	return length;
 }
 
+/**
+ * @brief Getter for the road Endpoint A position.
+ * @return The road Endpoint A position.
+*/
 glm::vec3 Object3Droad::getEndpointA() {
-	//Törlés utáni endpoint mozgatás index hibát okoz.
 	return basepoints[0];
 }
 
+/**
+ * @brief Getter for the road Endpoint B position.
+ * @return The road Endpoint B position.
+*/
 glm::vec3 Object3Droad::getEndpointB() {
 	return basepoints[3];
 }
 
+/**
+ * @brief Test the endpoints validity.
+*/
 void Object3Droad::endpointsExistTest() {
 	if (render->getDynamicObject(stickA) == NULL) {
 		stickA == -1;
@@ -165,6 +265,9 @@ void Object3Droad::endpointsExistTest() {
 	}
 }
 
+/**
+ * @brief Tester for the markers validity.
+*/
 void Object3Droad::markersExistTest() {
 	if (render->getDynamicObject(markerA) == NULL) {
 		markerA == -1;
@@ -174,9 +277,11 @@ void Object3Droad::markersExistTest() {
 	}
 }
 
+/**
+ * @brief Check the road endpoints sticking to an other road or endpoint marker.
+ * @param road 
+*/
 void Object3Droad::stuckTest(size_t road) {
-	//std::cout << "RUNNED" << std::endl;
-	//TODO Javasolt endpoint is deleted, marker is deleted hozzáadása ami törli a hibás út függõségeket.
 	endpointsExistTest();
 	if(glm::distance(render->getDynamicObject(road)->getEndpointA(), getEndpointA()) < shift + 1) {
 		stickA = road;
@@ -196,7 +301,12 @@ void Object3Droad::stuckTest(size_t road) {
 	}
 }
 
-char  Object3Droad::markerTest(size_t marker) {
+/**
+ * @brief Testing the marker sticking to a road endpoint.
+ * @param marker The testable marker ID.
+ * @return Return the sticked endpoint name.
+*/
+char Object3Droad::markerTest(size_t marker) {
 	markersExistTest();
 	char result = 'Q';
 	if ((glm::distance(getEndpointA(), render->getObject(marker)->getPosition()) < shift + 1) && endpointLock) {
@@ -214,6 +324,9 @@ char  Object3Droad::markerTest(size_t marker) {
 	return result;
 }
 
+/**
+ * @brief Set the road selected.
+*/
 void Object3Droad::select() {
 	setOpacity(0.5f);
 	for (size_t i = 0; i < 2; i++) render->getObject(roadEndCircles[i])->setOpacity(0.5f);
@@ -221,6 +334,9 @@ void Object3Droad::select() {
 	selected = true;
 }
 
+/**
+ * @brief Deselect the road.
+*/
 void Object3Droad::deselect() {
 	setOpacity(1.0f);
 	for (size_t i = 0; i < 2; i++) render->getObject(roadEndCircles[i])->setOpacity(1.0f);
@@ -228,6 +344,9 @@ void Object3Droad::deselect() {
 	selected = false;
 }
 
+/**
+ * @brief Generate the road one side points by a Bézier curve.
+*/
 void Object3Droad::generateRoadpoints() {
 	for (double u = 0.0; u <= 1.0; u += 0.01) {
 		points.push_back(this->bezierpoint(u));
@@ -246,6 +365,14 @@ void Object3Droad::generateRoadpoints() {
 	}
 }
 
+/**
+ * @brief Shift a point in Right angle to the point1 point2 section with currentShift in the yPosition level.
+ * @param point1  The section start point.
+ * @param point2 The section end point.
+ * @param currentShift The shift vector length.
+ * @param yPosition The Y level.
+ * @return The shifted point.
+*/
 glm::vec3 Object3Droad::shiftpoint(glm::vec3 point1, glm::vec3 point2, float currentShift, float yPosition) {
 	glm::vec3 shiftedPoint;
 	float xnormal = -1 * (point2.z - point1.z);
@@ -257,6 +384,11 @@ glm::vec3 Object3Droad::shiftpoint(glm::vec3 point1, glm::vec3 point2, float cur
 	return shiftedPoint;
 }
 
+/**
+ * @brief Generate a Bézier point bx u.
+ * @param u The parameter of the Bézier point.
+ * @return The generated Bézier point.
+*/
 glm::vec3 Object3Droad::bezierpoint(float u) {
 	glm::vec3 bezierPoint;
 	bezierPoint.x = pow(1.0f - u, 3.0f) * basepoints[0].x + 3.0f * u * pow(1.0f - u, 2.0f) * basepoints[1].x
@@ -267,6 +399,9 @@ glm::vec3 Object3Droad::bezierpoint(float u) {
 	return bezierPoint;
 }
 
+/**
+ * @brief Clean all the generated storages.
+*/
 void Object3Droad::clean() {
 	points.clear();
 	shiftedpoints_1.clear();
@@ -275,6 +410,9 @@ void Object3Droad::clean() {
 	trackTwo.clear();
 }
 
+/**
+ * @brief Fill the model coordinate storage.
+*/
 void Object3Droad::fillModelCoordinates() {
 	std::vector<glm::vec3> modelPoints;
 	for (size_t i = 0; i < points.size(); i++) {
@@ -285,6 +423,9 @@ void Object3Droad::fillModelCoordinates() {
 	modelCoordinates.BufferData(modelPoints);
 }
 
+/**
+ * @brief Fill the model normals.
+*/
 void Object3Droad::fillModelNormals() {
 	std::vector<glm::vec3> normals;
 	for (size_t i = 0; i < modelpointCount; i++) {
@@ -293,6 +434,9 @@ void Object3Droad::fillModelNormals() {
 	modelNormals.BufferData(normals);
 }
 
+/**
+ * @brief Fill the texture coordinates.
+*/
 void Object3Droad::fillModelTextures() {
 	std::vector<glm::vec2> textures;
 	for (size_t i = 0; i < modelpointCount; i = i + 4) {
@@ -304,6 +448,9 @@ void Object3Droad::fillModelTextures() {
 	modelTextures.BufferData(textures);
 }
 
+/**
+ * @brief Generate the 3D Vertexes.
+*/
 void Object3Droad::fillModelIndices() {
 	std::vector<int> indices;
 	indices.push_back(0);
@@ -325,7 +472,9 @@ void Object3Droad::fillModelIndices() {
 	modelIndices.BufferData(indices);
 }
 
-
+/**
+ * @brief Generate the current 3D Mosel.
+*/
 void Object3Droad::generate() {
 	clean();
 	generateRoadpoints();
@@ -340,14 +489,20 @@ void Object3Droad::generate() {
 	}, modelIndices);
 }
 
+/**
+ * @brief Update the road and recreate the model.
+*/
 void Object3Droad::update() {
-	//std::cout << "UPDATED " << getRenderID() << std::endl;
 	updateBasepoints();
 	generate();
 	render->getObject(roadEndCircles[0])->setPosition(glm::vec3(points[1].x, roadYposition + 0.05f, points[1].z));
 	render->getObject(roadEndCircles[1])->setPosition(glm::vec3(points[points.size() - 1].x, roadYposition + 0.05f, points[points.size() - 1].z));
 }
 
+/**
+ * @brief Set the endpoint lockers.
+ * @param lock Lock or free the endpoints.
+*/
 void Object3Droad::setEndpointLock(bool lock) {
 	endpointLock = lock;
 	stickA = -1;
@@ -360,14 +515,50 @@ void Object3Droad::setEndpointLock(bool lock) {
 	markerB = -1;
 }
 
+/**
+ * @brief Lock the editable mode.
+*/
 void Object3Droad::lockEditor() {
 	this->editorLock = true;
 	if (this->markerA != -1) this->render->getObject(this->markerA)->setSelectable(true);
 	if (this->markerB != -1) this->render->getObject(this->markerB)->setSelectable(true);
 }
 
+/**
+ * @brief Free the editable mode.
+*/
 void Object3Droad::freeEditor() {
 	this->editorLock = false;
-	//if (this->markerA != -1) this->render->getObject(this->markerA)->setSelectable(false);
-	//if (this->markerB != -1) this->render->getObject(this->markerB)->setSelectable(false);
 }
+
+/**
+ * @brief Getter the renderable road VAO.
+ * @return The renderable road VAO.
+*/
+VertexArrayObject& Object3Droad::getVAO() {
+	return model;
+};
+
+/**
+ * @brief Get the trackballs IDs.
+ * @return The trackballs IDs array.
+*/
+size_t* Object3Droad::getTrackBalls() {
+	return trackBalls;
+};
+
+/**
+ * @brief Get the endpoint circles IDs.
+ * @return The endpoint circles IDs.
+*/
+size_t* Object3Droad::getRoadEndCircles() {
+	return roadEndCircles;
+};
+
+/**
+ * @brief Get the base point coordinates.
+ * @return The base point coordinates in array.
+*/
+glm::vec3* Object3Droad::getBasepoints() {
+	return basepoints;
+};
--- TrafficSimulator/View/Objects/Object3Droad.h
+++ TrafficSimulator/View/Objects/Object3Droad.h
@@ -1,6 +1,18 @@
+/**
+ * @name Traffic Simulation
+ * @file Object3Droad.cpp
+ * @class Object3Droad
+ * @author Sándor Balázs - AZA6NL
+ * @date 2021.12.15.
+ * @brief The parametric generated road implementation.
+ * Contact: sandorbalazs9402@gmail.com
+*/
+
 //T'ana
+
 #pragma once
 
+//Empty lass for avoiding the circular includes.
 class Render;
 
 #include "Object3D.h"
@@ -8,121 +20,208 @@ class Render;
 #include "../Utilities/BufferObject.h"
 #include <vector>
 
+/**
+ * @brief The parametric generated road implementation.
+*/
 class Object3Droad {
 public:
+
+	/**
+	 * @brief The Object3Droad constructor. Set the basic variables too.
+	*/
 	Object3Droad();
+
+	/**
+	 * @brief Object3Droad copy constructor.
+	 * @param The copyable road.
+	*/
 	Object3Droad(const Object3Droad&);
+
+	/**
+	 * @brief Destructor.
+	 * @param Empty.
+	*/
 	~Object3Droad(void);
 
+	/**
+	 * @brief Bind this road to the render and initialize the road, generate the 3D model and initialize the connected objects.
+	 * @param render The current render pointer.
+	 * @param newRenderID The current render ID.
+	 * @param trackBall_0 The trackBall_0 object ID.
+	 * @param trackBall_1 The trackBall_1 object ID.
+	 * @param trackBall_2 The trackBall_2 object ID.
+	 * @param trackBall_3 The trackBall_3 object ID.
+	 * @param roadEndCircle_1 The roadEndCircle_1 object ID.
+	 * @param roadEndCircle_2 The roadEndCircle_2 object ID.
+	*/
 	void bind(Render* render, size_t newRenderID, size_t trackBall_0, size_t trackBall_1, size_t trackBall_2, size_t trackBall_3, size_t roadEndCircle_1, size_t roadEndCircle_2);
+
+	/**
+	 * @brief Rebind the connected objects after changes.
+	 * @param trackBall_0 The trackBall_0 object ID.
+	 * @param trackBall_1 The trackBall_1 object ID.
+	 * @param trackBall_2 The trackBall_2 object ID.
+	 * @param trackBall_3 The trackBall_3 object ID.
+	 * @param roadEndCircle_1 The roadEndCircle_1 object ID.
+	 * @param roadEndCircle_2 The roadEndCircle_2 object ID.
+	*/
 	void reBind(size_t trackBall_0, size_t trackBall_1, size_t trackBall_2, size_t trackBall_3, size_t roadEndCircle_1, size_t roadEndCircle_2);
 
+	/**
+	 * @brief Generate the current 3D Mosel.
+	*/
 	void generate();
+
+	/**
+	 * @brief Update the road and recreate the model.
+	*/
 	void update();
-	VertexArrayObject& getVAO() {
-		return model;
-	};
 
-	size_t* getTrackBalls() {
-		return trackBalls;
-	};
+	/**
+	 * @brief Getter the renderable road VAO.
+	 * @return The renderable road VAO.
+	*/
+	VertexArrayObject& getVAO();
 
-	size_t* getRoadEndCircles() {
-		return roadEndCircles;
-	};
+	/**
+	 * @brief Get the trackballs IDs.
+	 * @return The trackballs IDs array.
+	*/
+	size_t* getTrackBalls();
 
-	glm::vec3* getBasepoints() {
-		return basepoints;
-	};
+	/**
+	 * @brief Get the endpoint circles IDs.
+	 * @return The endpoint circles IDs.
+	*/
+	size_t* getRoadEndCircles();
+
+	/**
+	 * @brief Get the base point coordinates.
+	 * @return The base point coordinates in array.
+	*/
+	glm::vec3* getBasepoints();
+
+	size_t getRenderID();
+
+	void setRGBAcolor(glm::vec4 RGBAcolor);
+
+	void setRGBcolor(glm::vec3 RGBcolor);
+
+	void setOpacity(float opacity);
+
+	glm::mat4 getWorldMatrix();
+
+	glm::vec4 getRGBAcolor();
 
 
-	size_t 					getRenderID();
-	void					setRGBAcolor(glm::vec4 RGBAcolor);
-	void					setRGBcolor(glm::vec3 RGBcolor);
-	void					setOpacity(float opacity);
-	glm::mat4				getWorldMatrix();
-	glm::vec4				getRGBAcolor();
-	glm::vec3				getRGBcolor();
-	float					getOpacity();
-	bool					isSelected();
-	void					select();
-	void					deselect();
-	bool					isClicked(glm::vec3 cameraPosition, glm::vec3 ray);
-	glm::vec3				getEndpointA();
-	glm::vec3				getEndpointB();
-	void					stuckTest(size_t road);
-	char					markerTest(size_t marker);
-	void					setEndpointLock(bool lock);
-	void					lockEditor();
-	void					freeEditor();
+	glm::vec3 getRGBcolor();
+
+	float getOpacity();
+
+	bool isSelected();
+
+	void select();
+
+	void deselect();
+
+	bool isClicked(glm::vec3 cameraPosition, glm::vec3 ray);
+
+	glm::vec3 getEndpointA();
+
+	glm::vec3 getEndpointB();
+
+	void stuckTest(size_t road);
+
+	char markerTest(size_t marker);
+
+	void setEndpointLock(bool lock);
+
+	void lockEditor();
+
+	void freeEditor();
+
+	float getRoadLength();
 
 	std::vector<glm::vec3> points;
+
 	std::vector<glm::vec3> shiftedpoints_1;
+
 	std::vector<glm::vec3> shiftedpoints_2;
+
 	std::vector<glm::vec3> trackOne;
+
 	std::vector<glm::vec3> trackTwo;
 
 	size_t stickA = -1;
+
 	char stickMarkA = 'Q';
+
 	size_t stickB = -1;
+
 	char stickMarkB = 'Q';
 
 	size_t markerA = -1;
+
 	size_t markerB = -1;
 
 	size_t modelID = -1;
 
-	float getRoadLength();
-
 private:
+
 	void generateRoadpoints();
+
 	glm::vec3 bezierpoint(float u);
+
 	glm::vec3 shiftpoint(glm::vec3 point1, glm::vec3 point2, float currentShift, float yPosition);
+
 	void updateBasepoints();
+
 	void clean();
 
 	void endpointsExistTest();
+
 	void markersExistTest();
 
 	void fillModelCoordinates();
+
 	void fillModelNormals();
+
 	void fillModelTextures();
+
 	void fillModelIndices();
 
+
 	size_t renderID = -1;
+
 	bool selected = true;
+
 	bool endpointLock = true;
 	
 	glm::vec4 color;
 
-	/*size_t stickA = -1;
-	char stickMarkA = 'Q';
-	size_t stickB = -1;
-	char stickMarkB = 'Q';
-
-	size_t markerA = -1;
-	size_t markerB = -1;*/
-
 	bool editorLock = false;
 
 	float shift = 2.0f;
-	size_t trackBalls[4];
-	size_t roadEndCircles[2];
-	glm::vec3 basepoints[4];
-	float roadYposition;
 
-	/*std::vector<glm::vec3> points;
-	std::vector<glm::vec3> shiftedpoints_1;
-	std::vector<glm::vec3> shiftedpoints_2;
-	std::vector<glm::vec3> trackOne;
-	std::vector<glm::vec3> trackTwo;*/
+	size_t trackBalls[4];
+
+	size_t roadEndCircles[2];
+
+	glm::vec3 basepoints[4];
+
+	float roadYposition;
 
 	size_t	modelpointCount;
 
-	VertexArrayObject		model;
-	ArrayBuffer				modelCoordinates;
-	IndexBuffer				modelIndices;
-	ArrayBuffer				modelNormals;
-	ArrayBuffer				modelTextures;
-	Render*					render;
+	VertexArrayObject model;
+
+	ArrayBuffer modelCoordinates;
+
+	IndexBuffer modelIndices;
+
+	ArrayBuffer modelNormals;
+
+	ArrayBuffer modelTextures;
+
+	Render* render;
 };
