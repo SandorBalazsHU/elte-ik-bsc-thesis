12de7bf17345f088fdd6e0733edc0b04f59067d3
Balázs Sándor <sabuaai@INF.ELTE.HU>
2021-12-19 21:54:34 +0100

    Adding Doxy to reverseMap.

--- TrafficSimulator/Model/reverseMap.cpp
+++ TrafficSimulator/Model/reverseMap.cpp
@@ -1,14 +1,41 @@
+/**
+ * @name Traffic Simulation
+ * @file reverseMap.cpp
+ * @class TrafficSimulator
+ * @author Sándor Balázs - AZA6NL
+ * @date 2021.11.08.
+ * @brief The reverse map implementation for the Dijkstra algorithm.
+ * Contact: sandorbalazs9402@gmail.com
+*/
+
 #include "reverseMap.h"
 
+/**
+ * @brief Getter for a value by node.
+ * @param node The node.
+ * @return The value.
+*/
 int reverseMap::getValueByNode(size_t node) {
     return nodeIndexedStore[node];
 }
+
+/**
+ * @brief Set a value by node.
+ * @param node The node.
+ * @param newValue The new value.
+*/
 void reverseMap::setValueByNode(size_t node, int newValue) {
     int oldValue = nodeIndexedStore[node];
     nodeIndexedStore[node] = newValue;
     changeElementIndex(oldValue, newValue, node);
 }
 
+/**
+ * @brief Change the index of an element.
+ * @param oldIndex The old index.
+ * @param newIndex The new index.
+ * @param element The element.
+*/
 void reverseMap::changeElementIndex(const size_t oldIndex, const size_t newIndex, const int element) {
     std::vector<int> findedElements;
     auto it = valueIndexedStore.find(oldIndex);
@@ -26,6 +53,11 @@ void reverseMap::changeElementIndex(const size_t oldIndex, const size_t newIndex
     }
 }
 
+/**
+ * @brief Erase safely an element.
+ * @param node The node.
+ * @param value The value.
+*/
 void reverseMap::safeErase(size_t node, int value) {
     std::vector<int> findedElements;
     auto it = valueIndexedStore.find(value);
@@ -51,15 +83,29 @@ std::pair<size_t, int> reverseMap::removeMinimumValueElement() {
     return std::pair<size_t, int>(minimumNode, minimumValue);
 }
 
+/**
+ * @brief Insert an instance to the reverse map.
+ * @param node The node.
+ * @param value The value.
+*/
 void reverseMap::insert(size_t node, int value) {
     nodeIndexedStore.insert(std::pair<size_t, int>(node, value));
     valueIndexedStore.insert(std::pair<int, size_t>(value, node));
 }
 
+/**
+ * @brief The map is empty?
+ * @return The map is empty?
+*/
 bool reverseMap::empty() {
     return nodeIndexedStore.empty() && valueIndexedStore.empty();
 }
 
+/**
+ * @brief The node is in the map?
+ * @param node The node.
+ * @return Is in the map?
+*/
 bool reverseMap::isIn(size_t node) {
     return nodeIndexedStore.find(node) != nodeIndexedStore.end();
 }
--- TrafficSimulator/Model/reverseMap.h
+++ TrafficSimulator/Model/reverseMap.h
@@ -1,27 +1,99 @@
+/**
+ * @name Traffic Simulation
+ * @file reverseMap.h
+ * @class TrafficSimulator
+ * @author Sándor Balázs - AZA6NL
+ * @date 2021.11.08.
+ * @brief The reverse map implementation for the Dijkstra algorithm.
+ * Contact: sandorbalazs9402@gmail.com
+*/
+
 #pragma once
+
 #include <vector>
 #include <map>
 #include <set>
 
+/**
+ * @brief The reverse map implementation for the Dijkstra algorithm.
+*/
 class reverseMap {
 public:
+
+    /**
+     * @brief Constructor for the reverse map.
+    */
     reverseMap() {}
+
+    /**
+     * @brief Destructor for the reverse map.
+    */
     ~reverseMap() {}
+
+    /**
+     * @brief Getter for a value by node.
+     * @param node The node.
+     * @return The value.
+    */
     int getValueByNode(size_t node);
+
+    /**
+     * @brief Set a value by node.
+     * @param node The node.
+     * @param newValue The new value.
+    */
     void setValueByNode(size_t node, int newValue);
+
     /**
      * @brief Remove the minimum value element and return it
      * @return std::pair<node,value>
     */
     std::pair<size_t, int> removeMinimumValueElement();
+
+    /**
+     * @brief Insert an instance to the reverse map.
+     * @param node The node.
+     * @param value The value.
+    */
     void insert(size_t node, int value);
+
+    /**
+     * @brief The map is empty?
+     * @return The map is empty?
+    */
     bool empty();
+
+    /**
+     * @brief The node is in the map?
+     * @param node The node.
+     * @return Is in the map?
+    */
     bool isIn(size_t node);
+
+    /**
+     * @brief THe node index storage.
+    */
     std::map<size_t, int> nodeIndexedStore;
+
+    /**
+     * @brief The value index storage.
+    */
     std::multimap<int, size_t> valueIndexedStore;
+
 private:
-    //std::map<int, int> nodeIndexedStore;
-    //std::multimap<int, int> valueIndexedStore;
+
+    /**
+     * @brief Change the index of an element. 
+     * @param oldIndex The old index.
+     * @param newIndex The new index.
+     * @param element The element.
+    */
     void changeElementIndex(const size_t oldIndex, const size_t newIndex, const int element);
+
+    /**
+     * @brief Erase safely an element.
+     * @param node The node.
+     * @param value The value.
+    */
     void safeErase(size_t node, int value);
 };
