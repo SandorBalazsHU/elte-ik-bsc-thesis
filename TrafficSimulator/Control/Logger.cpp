bbdf6a5c7984d49a85d1b7f2f66f0e78a9e8795f
Balázs Sándor <sabuaai@INF.ELTE.HU>
2021-12-20 18:52:55 +0100

    Adding Doxy to the Logger.

--- TrafficSimulator/Control/Logger.cpp
+++ TrafficSimulator/Control/Logger.cpp
@@ -1,9 +1,22 @@
+/**
+ * @name Traffic Simulation
+ * @file Logger.cpp
+ * @class Logger
+ * @author Sándor Balázs - AZA6NL
+ * @date 2021.11.08.
+ * @brief Logging system implementation. (Static class.)
+ * Contact: sandorbalazs9402@gmail.com
+*/
+
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <ctime>
 #include <string>
 #include "Logger.h"
 
+/**
+ * @brief Start and initialize the file logging and open the log files.
+*/
 void Logger::startFileLogging() {
 	const bool logToFile = true;
 	if (logToFile) {
@@ -12,11 +25,18 @@ void Logger::startFileLogging() {
 	}
 }
 
+/**
+ * @brief Stop the file logging and close the log files.
+*/
 void Logger::stopFileLogging() {
 	fclose(stdout);
 	fclose(stderr);
 }
 
+/**
+ * @brief Get the current date and time in String.
+ * @return The current date and time.
+*/
 std::string Logger::currentDateTime() {
 	time_t now = time(0);
     tm* local_time = localtime(&now);
@@ -30,14 +50,27 @@ std::string Logger::currentDateTime() {
 	return dateTime;
 }
 
+/**
+ * @brief Log a message.
+ * @param message The log message.
+*/
 void Logger::log(std::string message) {
 	std::cout << currentDateTime() << " - " << message << std::endl;
 }
 
+/**
+ * @brief Log an error.
+ * @param message The error message.
+*/
 void Logger::error(std::string message) {
 	std::cerr << currentDateTime() << " - " << message << std::endl;
 }
 
+/**
+ * @brief Log an error and error code.
+ * @param message The error message.
+ * @param errorCode The error code.
+*/
 void Logger::error(std::string message, int errorCode) {
 	std::cerr << currentDateTime() << " - " << message << " ERROR CODE:" <<  errorCode << std::endl;
 }
--- TrafficSimulator/Control/Logger.h
+++ TrafficSimulator/Control/Logger.h
@@ -1,15 +1,62 @@
+/**
+ * @name Traffic Simulation
+ * @file Logger.h
+ * @class Logger
+ * @author Sándor Balázs - AZA6NL
+ * @date 2021.11.08.
+ * @brief Logging system implementation. (Static class.)
+ * Contact: sandorbalazs9402@gmail.com
+*/
+
 #pragma once
+
 #include <iostream>
 
+/**
+ * @brief Logging system implementation. (Static class.)
+*/
 class Logger {
 public:
+
+	/**
+	 * @brief Start and initialize the file logging and open the log files.
+	*/
 	static void startFileLogging();
+
+	/**
+	 * @brief Stop the file logging and close the log files.
+	*/
 	static void stopFileLogging();
+
+	/**
+	 * @brief Get the current date and time in String.
+	 * @return The current date and time.
+	*/
 	static std::string currentDateTime();
+
+	/**
+	 * @brief Log a message.
+	 * @param message The log message.
+	*/
 	static void log(std::string message);
+
+	/**
+	 * @brief Log an error.
+	 * @param message The error message.
+	*/
 	static void error(std::string message);
+
+	/**
+	 * @brief Log an error and error code.
+	 * @param message The error message.
+	 * @param errorCode The error code.
+	*/
 	static void error(std::string message, int errorCode);
 
 private:
+
+	/**
+	 * @brief The constructor is private and empty because is a static class.
+	*/
 	Logger() {}
 };
