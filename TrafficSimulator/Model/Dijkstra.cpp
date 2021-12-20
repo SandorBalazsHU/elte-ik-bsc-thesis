0cea38bd6f6fba9b1d6c9ef6f9904936a3fcf8ae
Balázs Sándor <sabuaai@INF.ELTE.HU>
2021-12-20 16:45:19 +0100

    Adding Doxy to the Dijkstra.

--- TrafficSimulator/Model/Dijkstra.cpp
+++ TrafficSimulator/Model/Dijkstra.cpp
@@ -1,14 +1,53 @@
+/**
+ * @name Traffic Simulation
+ * @file Dijkstra.cpp
+ * @class Dijkstra
+ * @author Sándor Balázs - AZA6NL
+ * @date 2021.11.08.
+ * @brief Dijkstra algorithm implementation.
+ * Contact: sandorbalazs9402@gmail.com
+*/
+
 #include "Dijkstra.h"
 
+/**
+ * @brief Dijkstra constructor.
+ * @param nodesCount The graph node count.
+ * @param startNode The graph start node.
+*/
+Dijkstra::Dijkstra(size_t nodesCount, size_t startNode) : nodesCount(nodesCount), startNode(startNode) {
+    init();
+}
+
+/**
+ * @brief Dijkstra destructor.
+*/
+Dijkstra::~Dijkstra() {}
+
+/**
+ * @brief Adding a new edge to the matrix.
+ * @param fromNode The new edge start node.
+ * @param toNode The new edge end node.
+ * @param weight The new edge weight.
+*/
 void Dijkstra::addEdge(size_t fromNode, size_t toNode, int weight) {
     matrix[fromNode][toNode] = weight;
 }
 
+/**
+ * @brief Adding a new double edge to the matrix.
+ * @param fromNode The new edge start node.
+ * @param toNode The new edge end node.
+ * @param weight The new edge weight.
+*/
 void Dijkstra::addDoubleEdge(size_t node1, size_t node2, int weight) {
     matrix[node1][node2] = weight;
     matrix[node2][node1] = weight;
 }
 
+/**
+ * @brief Initialize all node and data structures.
+*/
 void Dijkstra::init() {
     for (size_t i = 0; i < nodesCount; i++) {
         distance.push_back(infinite);
@@ -26,6 +65,9 @@ void Dijkstra::init() {
     from[startNode] = startNode;
 }
 
+/**
+ * @brief Preparation.
+*/
 void Dijkstra::prep() {
     ready.insert(startNode);
     distance[startNode] = 0;
@@ -34,6 +76,10 @@ void Dijkstra::prep() {
     from[startNode] = startNode;
 }
 
+/**
+ * @brief Initialize the next node.
+ * @param startNode The Initializeable node.
+*/
 void Dijkstra::initNext(size_t startNode) {
     for (size_t node = 0; node < nodesCount; node++) {
         if (matrix[startNode][node] != empty) {
@@ -42,16 +88,28 @@ void Dijkstra::initNext(size_t startNode) {
     }
 }
 
+/**
+ * @brief Initialize from.
+ * @param startNode Initializeable node.
+*/
 void Dijkstra::initFrom(size_t startNode) {
     for (size_t i = 0; i < nodesCount; i++) {
         if (matrix[startNode][i] != empty) from[i] = startNode;
     }
 }
 
+/**
+ * @brief Getter for the ready state.
+ * @param node The node.
+ * @return Is the node ready?
+*/
 bool Dijkstra::isReady(size_t node) {
     return ready.find(node) != ready.end();
 }
 
+/**
+ * @brief Run the algorithm and generate the result vector.
+*/
 void Dijkstra::run() {
     prep();
     while (!next.empty()) {
@@ -77,6 +135,10 @@ void Dijkstra::run() {
     }
 }
 
+/**
+  * @brief Getter for the start node.
+  * @return The start node id.
+ */
 size_t  Dijkstra::getStartNode() {
     return startNode;
 }
--- TrafficSimulator/Model/Dijkstra.h
+++ TrafficSimulator/Model/Dijkstra.h
@@ -1,40 +1,156 @@
+/**
+ * @name Traffic Simulation
+ * @file Dijkstra.h
+ * @class Dijkstra
+ * @author Sándor Balázs - AZA6NL
+ * @date 2021.11.08.
+ * @brief Dijkstra algorithm implementation.
+ * Contact: sandorbalazs9402@gmail.com
+*/
+
 #pragma once
+
 #include <vector>
 #include <map>
 #include <set>
 #include "reverseMap.h"
 
-
+/**
+ * @brief Dijkstra algorithm implementation.
+*/
 class Dijkstra {
 public:
-    Dijkstra(size_t nodesCount, size_t startNode) : nodesCount(nodesCount), startNode(startNode) {
-        init();
-    }
-    ~Dijkstra() {}
-    void addEdge(size_t fromNode, size_t toNode, int weight);
-    void addDoubleEdge(size_t node1, size_t node2, int weight);
-    size_t getStartNode();
-    //std::vector<std::vector<size_t>> getMatrix();
-    void run();
-    std::vector<size_t> from;
-    std::vector<std::vector<int>> matrix;
-private:
-    const int infinite = INT_MAX - 1;
-    const int empty = -1;
-    size_t nodesCount;
-    size_t startNode;
-    //std::vector<std::vector<size_t>> matrix;
-    std::set<size_t> ready;
-    std::vector<int> distance;
-    reverseMap next;
-    //std::vector<size_t> from;
+    /**
+     * @brief Dijkstra constructor.
+     * @param nodesCount The graph node count.
+     * @param startNode The graph start node.
+    */
+    Dijkstra(size_t nodesCount, size_t startNode);
 
+    /**
+     * @brief Dijkstra destructor.
+    */
+    ~Dijkstra();
+
+    /**
+     * @brief Adding a new edge to the matrix.
+     * @param fromNode The new edge start node.
+     * @param toNode The new edge end node.
+     * @param weight The new edge weight.
+    */
+    void addEdge(size_t fromNode, size_t toNode, int weight);
+
+    /**
+     * @brief Adding a new double edge to the matrix.
+     * @param fromNode The new edge start node.
+     * @param toNode The new edge end node.
+     * @param weight The new edge weight.
+    */
+    void addDoubleEdge(size_t node1, size_t node2, int weight);
+
+    /**
+     * @brief Getter for the start node.
+     * @return The start node id.
+    */
+    size_t getStartNode();
+
+    /**
+     * @brief Run the algorithm and generate the result vector.
+    */
+    void run();
+
+    /**
+     * @brief The algorithm result matrix.
+    */
+    std::vector<size_t> from;
+
+    /**
+     * @brief The graph representation matrix.
+    */
+    std::vector<std::vector<int>> matrix;
+
+private:
+
+    /**
+     * @brief Infinite mark.
+    */
+    const int infinite = INT_MAX - 1;
+
+    /**
+     * @brief Empty mark.
+    */
+    const int empty = -1;
+
+    /**
+     * @brief Node count.
+    */
+    size_t nodesCount;
+
+    /**
+     * @brief Start node.
+    */
+    size_t startNode;
+
+    /**
+     * @brief Ready nodes list.
+    */
+    std::set<size_t> ready;
+
+    /**
+     * @brief Distances list.
+    */
+    std::vector<int> distance;
+
+    /**
+     * @brief The next reverse map.
+    */
+    reverseMap next;
+
+    /**
+     * @brief Update the distance.
+     * @param node The updatble node.
+    */
     void updateDistance(size_t node);
+
+    /**
+     * @brief Initialize the next node.
+     * @param startNode The Initializeable node.
+    */
     void initNext(size_t startNode);
+
+    /**
+     * @brief Initialize from.
+     * @param startNode Initializeable node.
+    */
     void initFrom(size_t startNode);
+
+    /**
+     * @brief Getter for the next value.
+     * @param node The node.
+     * @return The next value.
+    */
     size_t getNextValue(size_t node);
+
+    /**
+     * @brief Update from.
+     * @param node The updatable node.
+    */
     void updateFrom(size_t node);
+
+    /**
+     * @brief Getter for the ready state.
+     * @param node The node.
+     * @return Is the node ready?
+    */
     bool isReady(size_t node);
+
+    /**
+     * @brief Initialize all node and data structures.
+    */
     void init();
+
+    /**
+     * @brief Preparation.
+    */
     void prep();
 };
